26024a09167e6b1e088bd22210c771e117913e3f
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-01 15:16:03 -0500

    Adding in a point for refactoring with a map Will come back once I get how maps work in this language

--- hello.go
+++ hello.go
@@ -11,6 +11,12 @@ const prefixInFrench = "Bonjour"
 const spanish = "Spanish"
 const french = "French"
 
+// const languages = map[string]string {
+// 	"Spanish": "Hola", 
+// 	"French": "Bonjour",
+// 	"English": "Hello",
+// }
+
 func Hello(name string, language string) string {
 	if "" == name {
 		return prefixInEnglish
@@ -30,6 +36,14 @@ func greetingPrefix(language string) (prefix string) {
 	}
 	return prefix
 }
+
+// func greetingPrefix2(language string) (prefix string) {
+// 	prefix, found := language[language]
+// 	if !found {
+// 		prefix = language["English"]
+// 	}
+// 	return prefix
+// }
 
 func main() {
 	fmt.Println(Hello("Nick", "English"))

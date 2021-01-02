1f812aeda5245c5830d4797026984f240c36d36c
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-01 21:03:26 -0500

    Added in a case for zero to make sure cases were covered

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,12 +3,23 @@ package iteration
 import "testing"
 
 func TestIteration(t *testing.T) {
-	repeated := Repeat("a", 5)
-	expected := "aaaaa"
-
-	if expected != repeated {
-		t.Errorf("Want %q got %q", expected, repeated)
+	assertSame := func(t *testing.T, expected string, repeated string) {
+		if expected != repeated {
+			t.Errorf("Want %q got %q", expected, repeated)
+		}
 	}
+
+	t.Run("Create a simple iteration", func (t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertSame(t, expected, repeated)
+	})
+
+	t.Run("Account for runs at zero", func(t *testing.T) {
+		repeated := Repeat("b", 0)
+		expected := ""
+		assertSame(t, expected, repeated)
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {

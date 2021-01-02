087badfd76f6f44c3531d5394de4fa930e15dcae
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-01 20:58:08 -0500

    Added in the code to allow the user to specify the amount of runs an interation should have. This does not count for values below zero or lower.

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, iterations int) string {
 	var result string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < iterations; i++ {
 		result += character
 	}
 	return result

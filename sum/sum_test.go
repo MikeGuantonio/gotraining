f4747380a272e01f3fc8e34c2b60ed872d5298bd
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-03 18:58:05 -0500

    Refactored test to use the t.Run ability.
    Working toward introducing slices into the mix

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,12 +3,18 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if want != got {
-		t.Errorf("Wanted %d got %d given %v", want, got, numbers)
+	assertIsEqual := func(t *testing.T, got int, want int, numbers [5]int) {
+		t.Helper()
+		if want != got {
+			t.Errorf("Wanted %d got %d given %v", want, got, numbers)
+		}
 	}
+	t.Run("Sum a list of integers", func(t *testing.T){
+		numbers := [5]int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+		assertIsEqual(t, got, want, numbers)
+	})
+	
 }

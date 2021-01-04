dff73913ebfec0fb6623a74d1fa8254b8e0eb705
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-03 19:37:30 -0500

    Refactored the code to account for an empty slice that will house the end result. So now instead of having to work with a single slice using make I can instead create a simple single slice and then use the append function to add in the values I want

--- sum/sum.go
+++ sum/sum.go
@@ -9,11 +9,10 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int{
-	totalCollectionsToSum := len(numbersToSum)
-	sums := make([]int, totalCollectionsToSum)
+	var sums []int
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))		
 	}
 	return sums
 }

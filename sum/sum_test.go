e6f4c4418c55c764f686e9a8005cd09a31880158
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-03 19:01:51 -0500

    Allowed for the updating of the numbers to sum slices only
    Removed the restriction on size of the array in processing

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -3,17 +3,27 @@ package sum
 import "testing"
 
 func TestSum(t *testing.T) {
-	assertIsEqual := func(t *testing.T, got int, want int, numbers [5]int) {
+	assertIsEqual := func(t *testing.T, got int, want int, numbers []int) {
 		t.Helper()
 		if want != got {
 			t.Errorf("Wanted %d got %d given %v", want, got, numbers)
 		}
 	}
+
 	t.Run("Sum a list of integers", func(t *testing.T){
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
+		assertIsEqual(t, got, want, numbers)
+	})
+
+	t.Run("Should sum a collection of any size", func(t *testing.T){
+		numbers := []int{1,2,3}
+
+		got := Sum(numbers)
+		want := 6
+
 		assertIsEqual(t, got, want, numbers)
 	})
 	

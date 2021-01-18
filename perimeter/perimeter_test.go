dfc6800550858bd570d9c47ecc389ebd9ffed184
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-17 20:05:57 -0500

    Go Train Area
    Added in an area function and its associated tests

--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -10,3 +10,12 @@ func TestPerimeter(t *testing.T) {
 		t.Errorf("Wanted %.2f, Got %.2f", want, got)
 	}
 }
+
+func TestArea(t *testing.T) {
+	got := Area(10.0, 10.0)
+	want := 100.0
+
+	if want != got {
+		t.Errorf("Wanted %.2f, got %.2f", want, got)
+	}
+}

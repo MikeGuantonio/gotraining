c2864a35f1b80f3c9c8702977ac7d8dafe496481
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-17 20:55:41 -0500

    Go Testng
    Added in table driven golang tests which are pretty cool. I just need to refactor for description

--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -13,22 +13,28 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
 
-		if want != got {
-			t.Errorf("Wanted %.2f, got %.2f", want, got)
-		}
+	areaTests := []struct {
+		description string
+		shape Shape
+		want float64
+	}{
+		{
+			"rectangle area",
+			Rectangle{12, 6},
+			72.0,
+		},
+        {
+			"circle area",
+			Circle{10},
+			314.1592653589793,
+		},
 	}
 
-	t.Run("rectangle area", func(t *testing.T){
-		rect := Rectangle{12.0, 6.0}
-		checkArea(t, rect, 72.0)
-	})
-
-	t.Run("circle area", func(t *testing.T){
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("Wanted %.2f, got %.2f", tt.want, got)
+		}
+	}
 }

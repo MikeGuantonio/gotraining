416095c7de213f4df68ba536f3e166daefe21e92
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-17 20:21:08 -0500

    Go Training Area And Perimeter
    Added in an area function and creating rich object oriented types.

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -1,8 +1,14 @@
 package perimeter
 
+const PI = 3.14
+
 type Rectangle struct {
 	Width float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 func Perimeter(rect Rectangle) float64{
@@ -11,4 +17,8 @@ func Perimeter(rect Rectangle) float64{
 
 func Area(rect Rectangle) float64 {
 	return rect.Width * rect.Height
+}
+
+func Area(circle Circle) float64 {
+	return circle.Radius * PI
 }
--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{12.0, 6.0}
-	got := Area(rect)
-	want := 72.0
+	t.Run("rectangle area", func(t *testing.T){
+		rect := Rectangle{12.0, 6.0}
+		got := rect.Area()
+		want := 72.0
 
-	if want != got {
-		t.Errorf("Wanted %.2f, got %.2f", want, got)
-	}
+		if want != got {
+			t.Errorf("Wanted %.2f, got %.2f", want, got)
+		}
+	})
+
+	t.Run("circle area", func(t *testing.T){
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if want != got {
+			t.Errorf("Got %g, Want %g", got, want)
+		}
+	})
 }

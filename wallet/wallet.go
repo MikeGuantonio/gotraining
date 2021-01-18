3e730e96b0b3de708f88aa532f24413554f187dc
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-18 10:21:46 -0500

    Go Test Training
    Refactored the tests to allow for an assertion of the balance. Also added a widthdraw function for the happy path. You can easily overdraft in this verison of the code.

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -20,3 +20,8 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
+
+func (w *Wallet) Widthdraw(amount Bitcoin) Bitcoin {
+	w.balance -= amount
+	return amount
+}
--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -2,20 +2,41 @@ package wallet
 
 import (
 	"testing" 
-	"fmt"
+	_"fmt"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}	
 
-	wallet.Deposit(Bitcoin(10))
-
-	got := wallet.Balance()
-	want := Bitcoin(10) 
-
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-
-	if got != want {
-		t.Errorf("Wanted %s, got %s", want, got)
+	assertBalance := func(t *testing.T, wallet  Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("Wanted %s, got %s", want, got)
+		}
 	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}	
+
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("widthdraw", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(20))
+
+		got := wallet.Widthdraw(Bitcoin(10))
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("Wanted %s, got %s", want, got)
+		}
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+
+	
 }

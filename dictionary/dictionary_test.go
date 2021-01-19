c44e9a429a8b0c9b75fa37b98afe0c52b531f6c6
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-19 08:19:34 -0500

    Go Test
    Refactored test code to allow for asserting a set of strings

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -8,6 +8,11 @@ func TestSearch(t *testing.T) {
 	got := Search(dictionary, "test")
 	want := "this is only a test"
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t *testing.T, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("Want %s, Got %s", want, got)
 	}

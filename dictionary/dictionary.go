2609f21a68c5ef273964e6db060e0116cee85aed
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-19 08:37:34 -0500

    Go Test
    Refactored the class to change value to defintion and leverage the maps's ability to return two values from its search operation.
    This allows me to check to see if something is found or not and then return a value based on that notion.

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -5,11 +5,11 @@ import "errors"
 type Dictionary map[string]string
 
 func (d Dictionary) Search(term string) (string, error) {
-	value := d[term]
+	definition, found := d[term]
 
-	if value == "" {
+	if !found {
 		return "", errors.New("could not find the word you were looking for")
 	}
 
-	return value, nil
+	return definition, nil
 }

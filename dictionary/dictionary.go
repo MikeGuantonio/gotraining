201929e90f91d73613a4a169708e19317ab9a43e
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-19 08:35:16 -0500

    Go Test
    Struggled with the syntax a little here.
    * Multiple return types are wrapped in () and can be assesed as a tuple on the recieving end. _ for not caring
    * You cannot return _ from a function. You must return nil
    * "" and nil are not the same. Based on the map definintion here if nothing is found it will return an empy string

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,7 +1,15 @@
 package dictionary
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(term string) string {
-	return d[term]
+func (d Dictionary) Search(term string) (string, error) {
+	value := d[term]
+
+	if value == "" {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return value, nil
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -3,12 +3,30 @@ package dictionary
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is only a test"}
 
-	got := dictionary.Search("test")
-	want := "this is only a test"
+	t.Run("Should find a word in the dictionary" ,func(t *testing.T){
+		dictionary := Dictionary{"test": "this is only a test"}
 
-	assertStrings(t, got, want)
+		got, _ := dictionary.Search("test")
+		want := "this is only a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("Should return an message to the user if no term is found", func(t *testing.T){
+		dictionary := Dictionary{"test": "this is only a test"}
+
+		_, err := dictionary.Search("west")
+		
+
+		if err == nil {
+			t.Fatal("Should recieve an error")
+		}
+
+		want := "could not find the word you were looking for"
+		assertStrings(t, err.Error(), want)
+	})
+	
 }
 
 func assertStrings(t *testing.T, got string, want string) {

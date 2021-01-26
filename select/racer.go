3346236d900ddfee437f2cc5a80e344bc542c7ea
Michael Guantonio <mike.guantonio@gmail.com>
2021-01-26 07:54:31 -0500

    Go Train Select
    Took the code out to DRY. Abstrated the duration measurement to a new function and abstracted mock server creation in tests. This allows us to now create some things on a fly should we want to give a list of urls

--- select/racer.go
+++ select/racer.go
@@ -6,16 +6,18 @@ import (
 )
 
 func Racer(player1 string, player2 string) (winner string){
-	startPlayer1 := time.Now()
-	http.Get(player1)
-	player1Duration := time.Since(startPlayer1)
-
-	startPlayer2 := time.Now()
-	http.Get(player2)
-	player2Duration := time.Since(startPlayer2)
+	player1Duration := measureResponseTime(player1)
+	player2Duration := measureResponseTime(player2)
 
 	if player1Duration < player2Duration {
 		return player1
 	}
 	return player2
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	duration := time.Since(start)
+	return duration
 }
--- select/racer_test.go
+++ select/racer_test.go
@@ -8,14 +8,8 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := createServer(20 * time.Millisecond)
+	fastServer := createServer(0)
 
 	slowURL := slowServer.URL
 	fastURL := fastServer.URL
@@ -26,4 +20,12 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("Wanted %v, Got %v", want, got)
 	}
+}
+
+func createServer(delay time.Duration) *httptest.Server{
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+	return server
 }

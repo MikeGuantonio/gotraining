f7dbb28978fea969ab091d6f9e2795a12026e4db
Michael Guantonio <mike.guantonio@gmail.com>
2021-02-02 08:27:30 -0500

    Go Train Reflection
    Okay cool so we can return reflect.Value to get our underlying type that we need to work with. This means that the type we are currenly needing will allow us to return the reflected value back from a fuction. This would be great in understanding how to get back the type of an object if that object should arise.

--- reflection/walk.go
+++ reflection/walk.go
@@ -2,12 +2,17 @@ package main
 
 import "reflect"
 
-func walk(x interface{}, fun func(input string)) {
-	val := reflect.ValueOf(x)
+func getValue(input interface{}) reflect.Value {
+	val := reflect.ValueOf(input)
 
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
+	return val
+}
+
+func walk(x interface{}, fun func(input string)) {
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)

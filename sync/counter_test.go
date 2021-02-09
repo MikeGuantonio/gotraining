97d4dba6d52e000f604b922b92290b8909a29161
Michael Guantonio <mike.guantonio@gmail.com>
2021-02-09 08:15:58 -0500

    Go Training Sync
    Just refactoring test to abstract the assertion and add in the test cases with t.Run(). No test cases yet.

--- sync/counter_test.go
+++ sync/counter_test.go
@@ -2,16 +2,21 @@ package main
 
 import "testing"
 
-func TestCounter(t *testing.T){
-	counter := Counter{}
-
-	counter.Inc()
-	counter.Inc()
-	counter.Inc()
-
-	want := 3
-	got := counter.Value()
-	if (got != want){
+func assertCount(t *testing.T, got Counter, want int){
+	t.Helper()
+	if (got.Value() != want){
 		t.Errorf("Got %d, Want %d", got, want)
 	}
+}
+
+func TestCounter(t *testing.T){
+	t.Run("Should do a basic increment", func(t *testing.T){
+		counter := Counter{}
+
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCount(t, counter, 3)
+	})
 }

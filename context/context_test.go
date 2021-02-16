3bc6bc0b59c85c73530e8075f0d8f9cc86418f74
Michael Guantonio <mike.guantonio@gmail.com>
2021-02-16 08:07:12 -0500

    Go Train Context
    We have added a new test for timing out a read by using go's context construct. The Context lives on the request and is helpful to have the server make decisions. If for whatever reason our context timesout we can look at our select and call store.Cancel(). This is helpful as it allows us to make the right steps forward when dealing with the context of a timeout.
    
    We are now leveraging goroutines here. We are getting data to the server. We are reading that data through a channel. The channel will listen in the go routine for data from store.Fetch(). This data will then be piped to the select channel to pull in the data that we expect. We then send it to the print writer and then back to the client. This is helpful for when we need to stop long running processes on the server by imposing a time out. We can pull out the request's context and have that context be closed without disturbing any of the main data processing.
    
    Spring handles this for us most of the time, it is kind of nice (and scary) that go allows us to manage this ourselves.

--- context/context_test.go
+++ context/context_test.go
@@ -2,28 +2,66 @@ package main
 
 import (
 	"testing"
-	"http/testing"
+	"net/http/httptest"
+	"net/http"
+	"time"
+	"context"
 )
 
-type StubStore struct {
+type SpyStore struct {
 	response string
+	cancelled bool
 }
 
-func(*ss StubStore) Fetch() string {
+func(ss *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return ss.response
 }
 
+func(ss *SpyStore) Cancel() {
+	ss.cancelled = true
+}
+
 func TestServer(t *testing.T) {
-	data := "hello halavanja"
-	store := StubStore{response: data}
-	server := Server(&store)
+	t.Run("Should send data from server", func(t *testing.T){
+		data := "hello halavanja"
+		store := SpyStore{response: data}
+		server := Server(&store)
 
-	request := httpTest.NewRequest(http.MethodGet, "/", nil)
-	response := httpTest.NewRecorder()
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
 
-	server.ServeHttp(response, request)
+		server.ServeHTTP(response, request)
 
-	if response.Body.String() != data {
-		t.Errorf("Wanted %s, Got %s", data, response.Body.String())
-	}
+		if response.Body.String() != data {
+			t.Errorf("Wanted %s, Got %s", data, response.Body.String())
+		}
+
+		if store.cancelled {
+			t.Errorf("Request should not have been cancelled due to timeout")
+		}
+	})
+
+	t.Run("Should cancel work for long running process", func(t *testing.T){
+		data := "hello halavanja"
+		store := SpyStore{response: data}
+		server := Server(&store)
+
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		// Create a context that will cancel after a time
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5 * time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+		
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Errorf("Request should have been cancelled due to timeout")
+		}
+	})
+	
 }
